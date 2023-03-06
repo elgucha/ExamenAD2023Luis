c26c7482892310a65a7ff4f06aa9f5248401442d
Luis  Gutiérrez Chamizo <92443528+elgucha@users.noreply.github.com>
2023-03-06 13:35:23 +0100

    Update Main.java

--- Ejercicio1AD2023/src/main/java/com/mycompany/ejercicio1ad2023/Main.java
+++ Ejercicio1AD2023/src/main/java/com/mycompany/ejercicio1ad2023/Main.java
@@ -19,19 +19,19 @@ public static void main(String[] args) {
     
     AlumnoDAO dao = new AlumnoDAO();
     
-    Alumno alumno1 = new Alumno(null, "Juan", "111111111", "juan@email.com", 9.0, 8.0);
+    Alumno alumno1 = new Alumno(null, "Pepita", "111111111", "pepita@email.com", 9.0, 8.0);
     dao.insertarAlumno(alumno1);
     
-    Alumno alumno2 = new Alumno(null, "Ana", "222222222", "ana@email.com", 7.5, 6.0);
+    Alumno alumno2 = new Alumno(null, "Rosita", "222222222", "rosita@email.com", 7.5, 6.0);
     dao.insertarAlumno(alumno2);
     
-    Alumno alumno3 = new Alumno(null, "Pedro", "333333333", "pedro@email.com", 6.0, 5.5);
+    Alumno alumno3 = new Alumno(null, "Hermina", "333333333", "hermina@email.com", 6.0, 5.5);
     dao.insertarAlumno(alumno3);
     
-    Alumno alumno4 = new Alumno(null, "Sara", "444444444", "sara@email.com", 8.5, 4.0);
+    Alumno alumno4 = new Alumno(null, "Aurelia", "444444444", "aurelia@email.com", 8.5, 4.0);
     dao.insertarAlumno(alumno4);
     
-    Alumno alumno5 = new Alumno(null, "Marta", "555555555", "marta@email.com", 9.0, 9.5);
+    Alumno alumno5 = new Alumno(null, "Anastasia", "555555555", "anastasia@email.com", 9.0, 9.5);
     dao.insertarAlumno(alumno5);
     
     dao.listarTodo();
